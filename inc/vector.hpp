b980309da49c0d59832a21e1a4fe082c2909c7b1
genouf <gabriel.enouf@yahoo.fr>
2022-12-20 16:29:55 +0100

    [SAVE] vector foundation

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -2,13 +2,15 @@
 # define VECTOR_HPP
 
 #include <iostream>
+#include "vector_iterator.hpp"
 
 namespace ft
 {
-	template<class T, class Allocator = std::allocator<T>>
+	template <class T, class Allocator = std::allocator<T>>
 	class vector
 	{
-		public:
+		public: 
+
 
 		/*		MEMBER TYPES		*/
 		typedef T value_type;
@@ -18,21 +20,63 @@ namespace ft
 		typedef typename allocator_type::const_reference const_reference;
 		typedef typename allocator_type::pointer pointer;
 		typedef typename allocator_type::const_pointer const_pointer;
-
-		value_type			iterator;
-		const value_type	const_iterator;
-		
+		typedef typename ft::vector_iterator iterator<T>;
+		typedef typename std::random_access_iterator_tag iterator_category;
+		typedef typename std::ptrdiff_t difference_type;
 
 		/*		CONSTRUCTORS		*/
-		explicit vector (const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0), v_pointer(NULL), alloc(al) {};
+		/*	Empty container constructor	*/
+		explicit vector (const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0), _container(NULL), _al(alloc) { return ; };
 		
-		explicit vector(size_type)
+		/*	Fill constructor	*/
+		explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _size(n), _capacity(n), _al(alloc)
+		{
+			this->_container = al.allocate(_capacity);
+			for (int i = 0; i < _size; i++)
+				_container[i] = val;
+			return ;
+		}
+
+		/*	Range constructor	*/
+		// template <class InputIterator>
+		// vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+		// {
+
+		// }
+
+		/*	Copy constructor	*/
+		vector (const vector& x) : _size(x._size), _capacity(x._capacity), _al(x._al)
+		{
+			this->_container = _al.allocate(_capacity);
+			for (int i = 0; i < size; i++)
+				_container[i] = x._container[i];
+			return (*this);
+		}
+
+		/*	DESTRUCTOR	*/
+		virtual ~vector()
+		{
+			_al.deallocate(_container, _capacity);
+			return ;
+		}
+
+		/*	OPERATORS	*/
+		vector& operator=(const vector& x)
+		{
+			if (*this != x)
+			{
+				this->_container = _al.allocate(_capacity);
+				for (int i = 0; i < size; i++)
+					_container[i] = x._container[i];
+			}
+			return (*this);
+		}
 
 		private:
 			value_type		_size;
 			value_type		_capacity;
-			value_type 		*_c_ptr;
-			allocator_type	alloc;
+			value_type 		*_container;
+			allocator_type	_al;
 	};
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,25 @@
 int	main(void)
 {
 	std::vector<int> v;
+	std::vector<int>::iterator it1;
+	std::vector<int>::iterator it2;
 
 	v.push_back(2);
 	v.push_back(7);
+	v.push_back(12);
+	v.push_back(23);
 
 	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it)
 		std::cout << *it;
 	std::cout << std::endl;
+	std::cout << "====== SUITE ======" << std::endl;
+	it1 = v.begin();
+	it2 = it1 + 2;
+	std::cout << "it1 = " << *it1 << std::endl;
+	std::cout << "it2 = " << *it2 << std::endl;
+	std::cout << std::endl << std::endl;
+	std::vector<int> v2(it1, it2);
+	for (std::vector<int>::iterator i = v2.begin(); i != v2.end(); i++)
+		std::cout << *i << std::endl;
 	return (0);
 }
